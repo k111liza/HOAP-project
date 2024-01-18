7d2673a56f433cc4533133b832154b350afae96a
Елизавета <crasovaliza@yandex.ru>
2024-01-18 18:20:24 +0300

    reformated code

--- scripts/windows.py
+++ scripts/windows.py
@@ -1,16 +1,19 @@
 from PyQt6 import uic
 from scripts.resource_path import resource_path
 
-class WindowBase:
-    windows=[]
-    def __init__(self, path):
 
+class WindowBase:
+    windows = []
+
+    def __init__(self, path):
         Form, Windows = uic.loadUiType(resource_path(path))
         self.window = Windows()
         self.form = Form()
         self.form.setupUi(self.window)
         WindowBase.windows.append(self)
+
     def open(self):
         self.window.show()
+
     def close(self):
         self.window.hide()
