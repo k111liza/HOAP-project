a76de31dbb59671a0127e87213a7a22f8afe7fe8
Елизавета <crasovaliza@yandex.ru>
2024-02-15 22:46:38 +0300

    done login

--- scripts/login.py
+++ scripts/login.py
@@ -7,20 +7,24 @@ class LoginWindow(WindowBase):
         super().__init__('designs/login.ui')
         self.form.enter.clicked.connect(self.enter)
         self.form.registration.clicked.connect(self.register)
+        self.form.loginerror.hide()
 
     def enter(self):
         login = self.form.login.text()
         password = self.form.password.text()
-        if login == 'admin' and password == 'admin':
+        data = WindowBase.db.get_login(login, password)
+        if data:
             self.window.hide()
+            WindowBase.windows[1].form.welcome.setText(f'Добро пожаловать, {data[1]}')
             WindowBase.windows[1].open()
+            WindowBase.user_id = int(data[0])
+            self.form.loginerror.hide()
             if self.form.rememberme.isChecked():
                 f = open(resource_path('../rememberme.txt'), 'w')
-                f.write(login)
+                f.write(str(data[0]))
                 f.close()
-
         else:
-            self.form.message.setText('Неверный логин или пароль')
+            self.form.loginerror.show()
     def register(self):
         self.window.hide()
         WindowBase.windows[2].open()
